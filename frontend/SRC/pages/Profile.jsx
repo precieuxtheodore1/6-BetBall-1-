ef5548da168e77935cc13fc683d0c83d323947f4
Théodore MUNANGA <mesressources7@gmail.com>
2025-09-21 16:34:31 +0200

    Update and rename Profile.js to Profile.jsx

---
+++ frontend/SRC/pages/Profile.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Profile() {
+  return (
+    <div style={{ padding: "20px" }}>
+      <h2>👤 Mon Profil</h2>
+      <p>Ici l’utilisateur verra son solde, ses paris et ses infos.</p>
+    </div>
+  );
+}
+
+export default Profile;
