620cc45831f97cccb1cf94307e95a865c4fbbf71
Théodore MUNANGA <mesressources7@gmail.com>
2025-09-21 16:03:11 +0200

    Update Navbar.js

--- frontend/SRC/components/Navbar.js
+++ frontend/SRC/components/Navbar.js
@@ -1,1 +1,19 @@
-
+import React from 'react';
+export default function Navbar({ onNavigate, user, onLogout }){
+  return (
+    <div className="nav">
+      <div style={{fontWeight:700,marginRight:10}}>6-BetBall</div>
+      <button onClick={()=>onNavigate('home')}>Accueil</button>
+      <button onClick={()=>onNavigate('games')}>Jeux</button>
+      <button onClick={()=>onNavigate('login')}>Connexion</button>
+      <button onClick={()=>onNavigate('register')}>Inscription</button>
+      <div style={{marginLeft:'auto'}}>
+        {user ? (<>
+          <span style={{marginRight:8}}>{user.username}</span>
+          <button onClick={()=>onNavigate('profile')}>Profil</button>
+          <button onClick={onLogout}>Déconnexion</button>
+        </>) : null}
+      </div>
+    </div>
+  );
+  }
