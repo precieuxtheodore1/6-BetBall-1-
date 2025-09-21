dc13d572c7dd296bdab33155de689a05955422e9
Théodore MUNANGA <mesressources7@gmail.com>
2025-09-21 16:32:19 +0200

    Update and rename Home.js to Home.jsx

---
+++ frontend/SRC/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Home() {
+  return (
+    <div style={{ padding: "20px" }}>
+      <h1>Bienvenue sur 6-BetBall 🎮</h1>
+      <p>Défiez vos amis et gagnez grâce à vos compétences, pas à la chance.</p>
+    </div>
+  );
+}
+
+export default Home;
